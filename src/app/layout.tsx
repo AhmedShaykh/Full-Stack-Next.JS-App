ea09b04a4a47f0e40f11aba0cf942603dc5aa14f
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-07-30 08:48:20 -0700

    Initial Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Full Stack Next.JS!",
+  description: "Full Stack Next.JS",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-slate-900 text-slate-200`}>
+        {children}
+      </body>
+    </html>
+  )
+};
