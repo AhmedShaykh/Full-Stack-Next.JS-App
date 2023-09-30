66cbc24e49203893a5911d48adadb5101852c274
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-09-30 22:02:45 +0500

    Add Login Page

--- src/Components/LoginPage.tsx
+++ src/Components/LoginPage.tsx
@@ -4,12 +4,11 @@ import { useRouter } from "next/navigation";
 import Link from "next/link";
 import axios from "axios";
 
-const RegisterPage = () => {
+const LoginPage = () => {
 
     const [user, setUser] = useState({
         email: "",
-        password: "",
-        username: "",
+        password: ""
     });
 
     const [buttonDisabled, setButtonDisabled] = useState(false);
@@ -18,28 +17,13 @@ const RegisterPage = () => {
 
     const router = useRouter();
 
-    const onSignup = async () => { };
+    const onLogin = async () => { };
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1 className="text-3xl font-semibold mt-3 mb-8">
-                {loading ? "Processing" : "Register"}
+                {loading ? "Processing" : "Login"}
             </h1>
-
-            <label
-                className="text-xl font-medium my-2"
-                htmlFor="username"
-            >
-                Username:
-            </label>
-            <input
-                className="p-2 w-[40%] border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
-                id="username"
-                type="text"
-                placeholder="username"
-                value={user.username}
-                onChange={(e) => setUser({ ...user, username: e.target.value })}
-            />
 
             <label
                 className="text-xl font-medium my-2"
@@ -73,16 +57,16 @@ const RegisterPage = () => {
 
             <button
                 className="py-2 px-5 rounded-lg my-4 font-bold text-black bg-white cursor-pointer"
-                onClick={onSignup}
+                onClick={onLogin}
             >
-                {buttonDisabled ? "No Signup" : "Signup"}
+                Login
             </button>
 
-            <Link href="/login" className="text-blue-700 text-xl cursor-pointer mt-3">
-                Visit Login Page
+            <Link href="/register" className="text-blue-700 text-xl cursor-pointer mt-3">
+                Visit Register Page
             </Link>
         </div>
     )
 };
 
-export default RegisterPage;
+export default LoginPage;
