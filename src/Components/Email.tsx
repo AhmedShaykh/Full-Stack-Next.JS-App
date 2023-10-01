7004adb1c0ace56436aa33c649ae88486c056434
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-10-02 02:14:20 +0500

    Final Commit

--- src/Components/Email.tsx
+++ src/Components/Email.tsx
@@ -1,8 +1,83 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
 
 const Email = () => {
+
+    const [token, setToken] = useState("");
+
+    const [verified, setVerified] = useState(false);
+
+    const [error, setError] = useState(false);
+
+    const verifyUserEmail = async () => {
+
+        try {
+
+            await fetch("/api/verifyemail", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    token
+                })
+            });
+
+            setVerified(true);
+
+        } catch (error: any) {
+
+            setError(true);
+
+            console.log(error.reponse.data);
+
+        }
+    };
+
+    useEffect(() => {
+
+        const urlToken = window.location.search.split("=")[1];
+
+        setToken(urlToken || "");
+
+    }, []);
+
+    useEffect(() => {
+
+        if (token.length > 0) {
+
+            verifyUserEmail();
+
+        }
+
+    }, [token]);
+
     return (
-        <div>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1 className="text-4xl">Verify Email</h1>
+
+            <h2 className="p-2 bg-cyan-700 rounded-md text-black my-6 font-semibold">
+                {token ? `${token}` : "No Token"}
+            </h2>
+
+            {verified && (
+                <div className="flex justify-center flex-col items-center gap-3">
+                    <h2 className="text-2xl">Email Verified</h2>
+
+                    <Link
+                        className="text-xl p-3 bg-white text-black font-bold rounded-lg"
+                        href="/login"
+                    >
+                        Login
+                    </Link>
+                </div>
+            )}
+            {error && (
+                <div className="flex justify-center">
+                    <h2 className="text-2xl bg-red-500 text-black">Error</h2>
+                </div>
+            )}
         </div>
     )
 };
